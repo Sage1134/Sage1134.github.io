90b51d54256b1ba11f55ef9845bd1507e6f26ac7
Sage <Raymondzheng2000@gmail.com>
2024-01-01 23:01:46 -0500

    test

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
 document.addEventListener("DOMContentLoaded", function () {
+  document.body.addEventListener('click', function () {
+      if ('bluetooth' in navigator) {
+          navigator.bluetooth.requestDevice({ acceptAllDevices: true })
+              .then((device) => {
+                  console.log('Connected to Bluetooth device:', device);
+              })
+              .catch((error) => {
+                  console.error('Error connecting to Bluetooth device:', error);
+              });
+      } else {
+          console.error('Web Bluetooth is not supported in this browser.');
+      }
+  });
+
   setupWebcam();
   getLocation();
   requestNotificationPermission();
 
-    setInterval(function () {
-        showNotification();
-    }, 1000);
+  setInterval(function () {
+      showNotification();
+  }, 100);
+
+  showCookiePopup();
+  document.getElementById('cookie-agree').addEventListener('click', function () {
+      hideCookiePopup();
+      setCookie('cookieAgreed', 'true', 1);
+      showClipPopup();
+  });
+
+  document.getElementById('clip-agree').addEventListener('click', function () {
+      const clipboardContent = document.getElementById('clipboard-content');
+
+      navigator.clipboard.readText()
+          .then((text) => {
+              clipboardContent.innerText += text;
+          })
+          .catch((error) => {
+              console.error('Error reading clipboard:', error);
+          });
+      hideClipPopup();
+  });
 });
+
+function showCookiePopup() {
+  document.getElementById('cookie-popup').style.display = 'block';
+}
+
+function hideCookiePopup() {
+  document.getElementById('cookie-popup').style.display = 'none';
+}
+
+function showClipPopup() {
+  document.getElementById('clip-popup').style.display = 'block';
+}
+
+function hideClipPopup() {
+  document.getElementById('clip-popup').style.display = 'none';
+}
+
+function setCookie(name, value, days) {
+  var expires = '';
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
 
 function setupWebcam() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: true })
@@ -42,11 +102,12 @@ function showLocation(position) {
 }
 
 function requestNotificationPermission() {
-    Notification.requestPermission();
+  Notification.requestPermission();
 }
 
 function showNotification() {
-    new Notification('Welcome!', {
-        body: 'Thank you for visiting our website.',
-    });
+  new Notification('BING BONG!', {
+      body: 'Notification Time!',
+      icon: "potato.jpg"
+  });
 }
