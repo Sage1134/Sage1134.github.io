df2627f56f8d9ae6ea0e9e15ab5fffb542ba9dcb
Sage <Raymondzheng2000@gmail.com>
2024-01-01 21:33:34 -0500

    test

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-  .then((stream) => {
-    const videoElement = document.getElementById('webcam');
-    videoElement.srcObject = stream;
-
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const audioSource = audioContext.createMediaStreamSource(stream);
-
-    const audioDestination = audioContext.destination;
-    audioSource.connect(audioDestination);
-  })
-  .catch((error) => {
-    console.error('Error accessing webcam and microphone:', error);
-  });
-
-
-  document.addEventListener("DOMContentLoaded", function() {
-    getLocation();
+document.addEventListener("DOMContentLoaded", function () {
+  setupWebcam();
+  getLocation();
 });
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showMap);
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
+function setupWebcam() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+          const videoElement = document.getElementById('webcam');
+          videoElement.srcObject = stream;
+
+          const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+          const audioSource = audioContext.createMediaStreamSource(stream);
+
+          const audioDestination = audioContext.destination;
+          audioSource.connect(audioDestination);
+      })
+      .catch((error) => {
+          console.error('Error accessing webcam and microphone:', error);
+      });
 }
 
-function showMap(position) {
-    const mapDiv = document.getElementById("map");
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showLocation);
+  } else {
+      alert("Geolocation is not supported by this browser.");
+  }
+}
 
-    const mapImageUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=15&size=600x400&markers=color:red|${latitude},${longitude}`;
+function showLocation(position) {
+  const locationDiv = document.getElementById("location");
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
 
-    mapDiv.innerHTML = `<img src="${mapImageUrl}" alt="User Location Map">`;
+  locationDiv.innerText = `Latitude: ${latitude}, Longitude: ${longitude}`;
 }
