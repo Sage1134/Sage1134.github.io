b3d3daa67d333a81cc9e5e6ea505865e6189b9a2
Sage <Raymondzheng2000@gmail.com>
2024-03-07 20:41:47 -0500

    updated all project pages

--- e.py
+++
@@ -1,6 +0,0 @@
-x = "string"
-y = "string"
-z = x
-print(id(x), id(y), id(z))
-print(x == y, x == z)
-print(x is y, x is z)
--- test.py
+++
@@ -1,7 +0,0 @@
-# Infinte loop with increasing i
-for i in enumerate(iter(int, 1)):
-    print(i[0])
-
-#Infinite loop with constant i
-for i in iter(int, 1):
-    print(i)
